a94206893ef110654696caf0b5e2bab78d9ed00e
AlexeyAxenov <85301524+AlexeyAxenov@users.noreply.github.com>
2022-03-20 12:50:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
+//commit
 public class Main {
 
     public static void main(String[] args) {
